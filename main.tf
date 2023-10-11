d76df9e499bddcf687fb9a3641dc4a05a15933dd
Dhandeaka Harris-Kearse <110075236+dhandeakaharriskearse@users.noreply.github.com>
2023-10-10 21:52:50 -0500

    #49 test our terrahome on terratowns test town (#50)

--- main.tf
+++ main.tf
@@ -22,29 +22,30 @@ required_providers {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid= var.teacherseat_user_uuid
+  token= var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  teacherseat_user_uuid = var.teacherseat_user_uuid
+  terratowns_access_token = var.terratowns_access_token
+  terratowns_endpoint = var.terratowns_endpoint
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home"{
   name = "A bomb recipe for rum cake!"
   description = <<DESCRIPTION
   This is a recipe I got from a friend's Grandmother. Don't tell her I'm giving this to you!
 DESCRIPTION
-  #domain_name = module.terrahous_aws.cloudfront_url
-  domain_name = "3fh59gz.cloudfront.net"
-  town = "cooker-cove"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
 
